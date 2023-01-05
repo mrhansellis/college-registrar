56481257c6a9d5f878549ea13045b301df606e87
Hans Ellis <bullhorn@gmail.com>
2023-01-04 20:15:52 -0800

    add courses controllers and courses views
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- Registrar/Controllers/CoursesController.cs
+++ Registrar/Controllers/CoursesController.cs
@@ -1,2 +1,77 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Registrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Registrar.Controllers
+{
+  public class CoursesController : Controller
+  {
+    private readonly RegistrarContext _db;
+    public CoursesController(RegistrarContext db)
+    {
+      _db = db;
+    }
+    
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+    
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult Details(int id)
+    {
+      Course thisCourse = _db.Courses
+        .Include(course => course.Students)
+        .ThenInclude(student => student.JoinEntities)
+        .ThenInclude(join => join.Student)
+        //.ThenInclude(join => join.Department)
+        .FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Courses.Update(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
