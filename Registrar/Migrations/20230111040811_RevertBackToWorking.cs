fea063be2c4c01e0d14e6aa77bef7c213b561e6e
Hans Ellis <bullhorn@gmail.com>
2023-01-10 20:33:14 -0800

    create revert to working migration
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

---
+++ Registrar/Migrations/20230111040811_RevertBackToWorking.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+#nullable disable
+
+namespace Registrar.Migrations
+{
+    public partial class RevertBackToWorking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
