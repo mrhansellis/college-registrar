005da65a8a589cfc1ce16a1e11febfbc4be00729
Hans Ellis <bullhorn@gmail.com>
2023-01-15 14:09:28 -0800

    add account controller with index and register actions
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

---
+++ Registrar/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Registrar.Models;
+using System.Threading.Tasks;
+using Registrar.ViewModels;
+
+namespace Registrar.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly RegistrarContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RegistrarContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
+    }
+  }
+}
